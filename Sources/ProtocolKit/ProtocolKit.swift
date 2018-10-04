b829e59b5eace9d94de1e9931f3da579345dfad9
Sam <samgauck@icloud.com>
2018-10-04 12:01:41 -0400

    Add documentation

--- Sources/ProtocolKit/ProtocolKit.swift
+++ Sources/ProtocolKit/ProtocolKit.swift
@@ -1,3 +1,7 @@
+/// A protocol that requires any implementers to have a `copy()` method.
 public protocol Copiable {
+    /// Returns a new instance that is a copy of this object.
+    ///
+    /// - Returns: A new instance that is a copy of the calling object.
     func copy() -> Self
 }
